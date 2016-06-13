300f83173477e37060f153d8a67caff6866d884e
HosungPark <indra622@gmail.com>
2016-06-13 18:36:26 +0900

    debug and softcoding completed

--- pklgz.py
+++ pklgz.py
@@ -31,74 +31,110 @@ def dir_to_dataset(glob_files):
 
 # 클래스별 데이터 삽입
 Data = dir_to_dataset("bike_resize\\*.jpg")
+trn_size = 588
+val_size = 168
+val_idx = trn_size+1+val_size
+data_size = 840
 # Data and labels are read 
 
 train_set_y = list()
 val_set_y = list()
 test_set_y = list()
-train_set_x = Data[:500]
-val_set_x = Data[501:600]
-test_set_x = Data[601:840]
-for i in range(500):
+train_set_x = Data[:trn_size]
+val_set_x = Data[trn_size+1:val_idx]
+test_set_x = Data[val_idx+1:data_size]
+for i in range(trn_size):
 	train_set_y.append('0')
-for i in range(100):
+for i in range(val_size):
 	val_set_y.append('0')
-for i in range(240):
+for i in range(val_size):
 	test_set_y.append('0')
 # Divided dataset into 3 parts. I had 6281 images.
 
 Data = dir_to_dataset("bus_resize\\*.jpg")
 # Data and labels are read 
+trn_size = 1638
+val_size = 546
+val_idx = trn_size+1+val_size
+data_size = 2730
+# Data and labels are read 
 
-train_set_x += Data[:500]
-val_set_x += Data[501:600]
-test_set_x += Data[601:840]
-for i in range(500):
+train_set_y = list()
+val_set_y = list()
+test_set_y = list()
+train_set_x = Data[:trn_size]
+val_set_x = Data[trn_size+1:val_idx]
+test_set_x = Data[val_idx+1:data_size]
+for i in range(trn_size):
     train_set_y.append('1')
-for i in range(100):
+for i in range(val_size):
     val_set_y.append('1')
-for i in range(240):
     test_set_y.append('1')
+
 
 Data = dir_to_dataset("sedan_resize\\*.jpg")
 # Data and labels are read 
 
-train_set_x += Data[:500]
-val_set_x += Data[501:600]
-test_set_x += Data[601:840]
-for i in range(500):
+trn_size = 15498
+val_size = 5166
+val_idx = trn_size+1+val_size
+data_size = 25830
+# Data and labels are read 
+
+train_set_y = list()
+val_set_y = list()
+test_set_y = list()
+train_set_x = Data[:trn_size]
+val_set_x = Data[trn_size+1:val_idx]
+test_set_x = Data[val_idx+1:data_size]
+for i in range(trn_size):
     train_set_y.append('2')
-for i in range(100):
+for i in range(val_size):
     val_set_y.append('2')
-for i in range(240):
     test_set_y.append('2')
 
 
 Data = dir_to_dataset("truck_resize\\*.jpg")
 # Data and labels are read 
 
-train_set_x += Data[:500]
-val_set_x += Data[501:600]
-test_set_x += Data[601:840]
-for i in range(500):
+trn_size = 7308
+val_size = 2436
+val_idx = trn_size+1+val_size
+data_size = 12180
+# Data and labels are read 
+
+train_set_y = list()
+val_set_y = list()
+test_set_y = list()
+train_set_x = Data[:trn_size]
+val_set_x = Data[trn_size+1:val_idx]
+test_set_x = Data[val_idx+1:data_size]
+for i in range(trn_size):
     train_set_y.append('3')
-for i in range(100):
+for i in range(val_size):
     val_set_y.append('3')
-for i in range(240):
     test_set_y.append('3')
 
 
 Data = dir_to_dataset("van_resize\\*.jpg")
 # Data and labels are read 
 
-train_set_x += Data[:500]
-val_set_x += Data[501:600]
-test_set_x += Data[601:840]
-for i in range(500):
+trn_size = 13860
+val_size = 4620
+val_idx = trn_size+1+val_size
+data_size = 23100
+# Data and labels are read 
+
+train_set_y = list()
+val_set_y = list()
+test_set_y = list()
+train_set_x = Data[:trn_size]
+val_set_x = Data[trn_size+1:val_idx]
+test_set_x = Data[val_idx+1:data_size]
+for i in range(trn_size):
     train_set_y.append('4')
-for i in range(100):
+for i in range(val_size):
     val_set_y.append('4')
-for i in range(240):
     test_set_y.append('4')
 
 # Data, y = dir_to_dataset("bus_resize\\*.jpg")

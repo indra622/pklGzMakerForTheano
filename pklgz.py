071f642306d9c367e0a79643828343bbbe78a3f8
HosungPark <indra622@gmail.com>
2016-06-13 19:00:17 +0900

    debug error

--- pklgz.py
+++ pklgz.py
@@ -59,12 +59,9 @@ val_idx = trn_size+1+val_size
 data_size = 2730
 # Data and labels are read 
 
-train_set_y = list()
-val_set_y = list()
-test_set_y = list()
-train_set_x = Data[:trn_size]
-val_set_x = Data[trn_size+1:val_idx]
-test_set_x = Data[val_idx+1:data_size]
+train_set_x += Data[:trn_size]
+val_set_x += Data[trn_size+1:val_idx]
+test_set_x += Data[val_idx+1:data_size]
 for i in range(trn_size):
     train_set_y.append('1')
 for i in range(val_size):
@@ -81,12 +78,9 @@ val_idx = trn_size+1+val_size
 data_size = 25830
 # Data and labels are read 
 
-train_set_y = list()
-val_set_y = list()
-test_set_y = list()
-train_set_x = Data[:trn_size]
-val_set_x = Data[trn_size+1:val_idx]
-test_set_x = Data[val_idx+1:data_size]
+train_set_x += Data[:trn_size]
+val_set_x += Data[trn_size+1:val_idx]
+test_set_x += Data[val_idx+1:data_size]
 for i in range(trn_size):
     train_set_y.append('2')
 for i in range(val_size):
@@ -103,12 +97,9 @@ val_idx = trn_size+1+val_size
 data_size = 12180
 # Data and labels are read 
 
-train_set_y = list()
-val_set_y = list()
-test_set_y = list()
-train_set_x = Data[:trn_size]
-val_set_x = Data[trn_size+1:val_idx]
-test_set_x = Data[val_idx+1:data_size]
+train_set_x += Data[:trn_size]
+val_set_x += Data[trn_size+1:val_idx]
+test_set_x += Data[val_idx+1:data_size]
 for i in range(trn_size):
     train_set_y.append('3')
 for i in range(val_size):
@@ -126,28 +117,16 @@ val_idx = trn_size+1+val_size
 data_size = 23100
 # Data and labels are read 
 
-train_set_y = list()
-val_set_y = list()
-test_set_y = list()
-train_set_x = Data[:trn_size]
-val_set_x = Data[trn_size+1:val_idx]
-test_set_x = Data[val_idx+1:data_size]
+train_set_x += Data[:trn_size]
+val_set_x += Data[trn_size+1:val_idx]
+test_set_x += Data[val_idx+1:data_size]
 for i in range(trn_size):
     train_set_y.append('4')
 for i in range(val_size):
     val_set_y.append('4')
     test_set_y.append('4')
 
-# Data, y = dir_to_dataset("bus_resize\\*.jpg")
-# # Data and labels are read 
 
-# train_set_x = Data[:2093]
-# val_set_x = Data[2094:4187]
-# test_set_x = Data[4188:6281]
-# train_set_y = y[:2093]
-# val_set_y = y[2094:4187]
-# test_set_y = y[4188:6281]
-# Divided dataset into 3 parts. I had 6281 images.
 train_set = train_set_x, train_set_y
 val_set = val_set_x, val_set_y
 test_set = test_set_x, val_set_y

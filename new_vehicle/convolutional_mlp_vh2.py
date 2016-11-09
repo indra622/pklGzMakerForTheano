aa10039ca1682595502e756ff40c2424a5f8680a
jeunsom <jeunsom2@naver.com>
2016-11-10 08:55:54 +0900

    other version
    it will be tested

---
+++ new_vehicle/convolutional_mlp_vh2.py
@@ -0,0 +1,330 @@
+"""This tutorial introduces the LeNet5 neural network architecture
+using Theano.  LeNet5 is a convolutional neural network, good for
+classifying images. This tutorial shows how to build the architecture,
+and comes with all the hyper-parameters you need to reproduce the
+paper's MNIST results.
+
+
+This implementation simplifies the model in the following ways:
+
+ - LeNetConvPool doesn't implement location-specific gain and bias parameters
+ - LeNetConvPool doesn't implement pooling by average, it implements pooling
+   by max.
+ - Digit classification is implemented with a logistic regression rather than
+   an RBF network
+ - LeNet5 was not fully-connected convolutions at second layer
+
+References:
+ - Y. LeCun, L. Bottou, Y. Bengio and P. Haffner:
+   Gradient-Based Learning Applied to Document
+   Recognition, Proceedings of the IEEE, 86(11):2278-2324, November 1998.
+   http://yann.lecun.com/exdb/publis/pdf/lecun-98.pdf
+
+"""
+
+from __future__ import print_function
+
+import os
+import sys
+import timeit
+
+import numpy
+
+import theano
+import theano.tensor as T
+from theano.tensor.signal import downsample
+from theano.tensor.nnet import conv2d
+
+from logistic_sgd import LogisticRegression, load_data
+#from mlp import HiddenLayer
+from cnn_tools import HiddenLayer, _dropout_from_layer, DropoutHiddenLayer, LeNetConvPoolLayer2, LeNetConvPoolLayer, DropoutLeNetConvPoolLayer,LeNetConvPoolLayer1
+	
+def evaluate_lenet5(learning_rate=0.01, n_epochs=2000,
+                    dataset='file_622.pkl.gz',
+					display_filters = True,
+                    nkerns=[32, 48, 64, 128, 256], batch_size=500): #batch_size = 500
+    """ Demonstrates lenet on MNIST dataset
+
+    :type learning_rate: float
+    :param learning_rate: learning rate used (factor for the stochastic
+                          gradient)
+
+    :type n_epochs: int
+    :param n_epochs: maximal number of epochs to run the optimizer
+
+    :type dataset: string
+    :param dataset: path to the dataset used for training /testing (MNIST here)
+
+    :type nkerns: list of ints
+    :param nkerns: number of kernels on each layer
+    """
+
+    rng = numpy.random.RandomState(23455)
+
+    datasets = load_data(dataset)
+
+    train_set_x, train_set_y = datasets[0]
+    valid_set_x, valid_set_y = datasets[1]
+    test_set_x, test_set_y = datasets[2]
+
+    # compute number of minibatches for training, validation and testing
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0]
+    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0]
+    n_test_batches = test_set_x.get_value(borrow=True).shape[0]
+    n_train_batches //= batch_size
+    n_valid_batches //= batch_size
+    n_test_batches //= batch_size
+
+    # allocate symbolic variables for the data
+    index = T.lscalar()  # index to a [mini]batch
+
+    # start-snippet-1
+    x = T.matrix('x')   # the data is presented as rasterized images
+    y = T.ivector('y')  # the labels are presented as 1D vector of
+                        # [int] labels
+
+    ######################
+    # BUILD ACTUAL MODEL #
+    ######################
+    print('... building the model')
+
+    # Reshape matrix of rasterized images of shape (batch_size, 28 * 28)
+    # to a 4D tensor, compatible with our LeNetConvPoolLayer
+    # (28, 28) is the size of MNIST images.
+    layer0_input = x.reshape((batch_size, 1, 100, 46)) #28,28
+
+    # Construct the first convolutional pooling layer:
+    # filtering reduces the image size to (100-3+1 , 46-3+1) = (98, 44)
+    # maxpooling reduces this further to (98/2, 44/2) = (49, 22)
+    # 4D output tensor is thus of shape (batch_size, nkerns[0], 49, 22)
+    layer0 = LeNetConvPoolLayer1(
+        rng,
+        input=layer0_input,
+        image_shape=(batch_size, 1, 100, 46),
+        filter_shape=(nkerns[0], 1, 3, 3),
+        poolsize=(2, 2)
+    )
+	
+    layer0_1 = LeNetConvPoolLayer( #Conv ReLU
+        rng,
+        input=layer0.output,
+        image_shape=(batch_size, nkerns[0], 49, 22),
+        filter_shape=(nkerns[1], nkerns[0], 3, 3),
+        poolsize=(2, 2)
+    )
+	
+    layer0_2 = LeNetConvPoolLayer( #Conv ReLU
+        rng,
+        input=layer0_1.output,
+        image_shape=(batch_size, nkerns[1], 47, 20),
+        filter_shape=(nkerns[1], nkerns[1], 3, 3),
+        poolsize=(2, 2)
+    )
+	
+    layer0_3 = LeNetConvPoolLayer( #Conv ReLU
+        rng,
+        input=layer0_2.output,
+        image_shape=(batch_size, nkerns[1], 45, 18),
+        filter_shape=(nkerns[2], nkerns[1], 3, 3),
+        poolsize=(2, 2)
+    )
+	
+    layer1 = LeNetConvPoolLayer( #Conv ReLU
+        rng,
+        input=layer0_3.output,
+        image_shape=(batch_size, nkerns[2], 43, 16),
+        filter_shape=(nkerns[2], nkerns[2], 3, 3),
+        poolsize=(2, 2)
+    )
+
+	
+    layer1_1 = LeNetConvPoolLayer( #Conv ReLU
+        rng,
+        input=layer1.output,
+        image_shape=(batch_size, nkerns[2], 41, 14),
+        filter_shape=(nkerns[3], nkerns[2], 3, 3),
+        poolsize=(2, 2)
+    )
+	
+    layer1_2 = LeNetConvPoolLayer( #Conv ReLU
+        rng,
+        input=layer1_1.output,
+        image_shape=(batch_size, nkerns[3], 39, 12),
+        filter_shape=(nkerns[3], nkerns[3], 3, 3),
+        poolsize=(2, 2)
+    )
+	
+    layer1_3 = LeNetConvPoolLayer( #Conv ReLU
+        rng,
+        input=layer1_2.output,
+        image_shape=(batch_size, nkerns[3], 37, 10),
+        filter_shape=(nkerns[3], nkerns[3], 3, 3),
+        poolsize=(2, 2)
+    )
+	
+    # Construct the second convolutional pooling layer
+    # filtering reduces the image size to (35-5+1, 8-5+1) = (31, 4)
+    # maxpooling reduces this further to (31/2, 4/2) = (15, 2)
+    # 4D output tensor is thus of shape (batch_size, nkerns[1], 15, 2)
+    layer2_1 = LeNetConvPoolLayer1(
+        rng,
+        input=layer1_3.output,
+        image_shape=(batch_size, nkerns[3], 35, 8),
+        filter_shape=(nkerns[4], nkerns[3], 5, 5),
+        poolsize=(2, 2)
+    )
+    
+    # the HiddenLayer being fully-connected, it operates on 2D matrices of
+    # shape (batch_size, num_pixels) (i.e matrix of rasterized images).
+    # This will generate a matrix of shape (batch_size, nkerns[1] * 22 * 8),
+    # or (500, 50 * 22 * 8) = (500, 8800) with the default values.
+    layer2_input = layer2_1.output.flatten(2)
+
+    # construct a fully-connected sigmoidal layer
+    layer2 = HiddenLayer(
+        rng,
+        input=layer2_input,
+        n_in=nkerns[4] *  15 * 2,
+        n_out=800,
+        activation=T.tanh
+    )
+
+    # classify the values of the fully-connected sigmoidal layer
+    layer3 = LogisticRegression(input=layer2.output, n_in=800, n_out=5)
+
+    # the cost we minimize during training is the NLL of the model
+    cost = layer3.negative_log_likelihood(y)
+
+    # create a function to compute the mistakes that are made by the model
+    test_model = theano.function(
+        [index],
+        layer3.errors(y),
+        givens={
+            x: test_set_x[index * batch_size: (index + 1) * batch_size],
+            y: test_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+
+    validate_model = theano.function(
+        [index],
+        layer3.errors(y),
+        givens={
+            x: valid_set_x[index * batch_size: (index + 1) * batch_size],
+            y: valid_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+
+    # create a list of all model parameters to be fit by gradient descent
+    params = layer3.params + layer2_1.params + layer1_3.params + layer1_2.params + layer1_1.params + layer1.params+ layer0_3.params  + layer0_2.params + layer0_1.params + layer0.params
+
+    # create a list of gradients for all model parameters
+    grads = T.grad(cost, params)
+
+    # train_model is a function that updates the model parameters by
+    # SGD Since this model has many parameters, it would be tedious to
+    # manually create an update rule for each model parameter. We thus
+    # create the updates list by automatically looping over all
+    # (params[i], grads[i]) pairs.
+    updates = [
+        (param_i, param_i - learning_rate * grad_i)
+        for param_i, grad_i in zip(params, grads)
+    ]
+
+    train_model = theano.function(
+        [index],
+        cost,
+        updates=updates,
+        givens={
+            x: train_set_x[index * batch_size: (index + 1) * batch_size],
+            y: train_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+    # end-snippet-1
+
+    ###############
+    # TRAIN MODEL #
+    ###############
+    print('... training')
+    # early-stopping parameters
+    patience = 10000  # look as this many examples regardless
+    patience_increase = 2  # wait this much longer when a new best is
+                           # found
+    improvement_threshold = 0.995  # a relative improvement of this much is
+                                   # considered significant
+    validation_frequency = min(n_train_batches, patience // 2)
+                                  # go through this many
+                                  # minibatche before checking the network
+                                  # on the validation set; in this case we
+                                  # check every epoch
+
+    best_validation_loss = numpy.inf
+    best_iter = 0
+    test_score = 0.
+    start_time = timeit.default_timer()
+
+    epoch = 0
+    done_looping = False
+
+    while (epoch < n_epochs) and (not done_looping):
+        epoch = epoch + 1
+        for minibatch_index in range(n_train_batches):
+
+            iter = (epoch - 1) * n_train_batches + minibatch_index
+
+            if iter % 100 == 0:
+                print('training @ iter = ', iter)
+            cost_ij = train_model(minibatch_index)
+
+            if (iter + 1) % validation_frequency == 0:
+
+                # compute zero-one loss on validation set
+                validation_losses = [validate_model(i) for i
+                                     in range(n_valid_batches)]
+                this_validation_loss = numpy.mean(validation_losses)
+                print('epoch %i, minibatch %i/%i, validation error %f %%' %
+                      (epoch, minibatch_index + 1, n_train_batches,
+                       this_validation_loss * 100.))
+
+                # if we got the best validation score until now
+                if this_validation_loss < best_validation_loss:
+
+                    #improve patience if loss improvement is good enough
+                    if this_validation_loss < best_validation_loss *  \
+                       improvement_threshold:
+                        patience = max(patience, iter * patience_increase)
+
+                    # save best validation score and iteration number
+                    best_validation_loss = this_validation_loss
+                    best_iter = iter
+
+                    # test it on the test set
+                    test_losses = [
+                        test_model(i)
+                        for i in range(n_test_batches)
+                    ]
+                    test_score = numpy.mean(test_losses)
+                    print(('     epoch %i, minibatch %i/%i, test error of '
+                           'best model %f %%') %
+                          (epoch, minibatch_index + 1, n_train_batches,
+                           test_score * 100.))
+
+            """if patience <= iter:
+                done_looping = True
+                break"""
+
+    end_time = timeit.default_timer()
+    print('Optimization complete.')
+    print('Best validation score of %f %% obtained at iteration %i, '
+          'with test performance %f %%' %
+          (best_validation_loss * 100., best_iter + 1, test_score * 100.))
+    print(('The code for file ' +
+           os.path.split(__file__)[1] +
+           ' ran for %.2fm' % ((end_time - start_time) / 60.)), file=sys.stderr)
+
+
+if __name__ == '__main__':
+    evaluate_lenet5()
+
+
+def experiment(state, channel):
+    evaluate_lenet5(state.learning_rate, dataset=state.dataset)
